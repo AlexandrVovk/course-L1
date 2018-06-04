6f16e289b0e1078c1a68ebfd28ed93960ea7d21a
alexandr.vovk <alexandr.vovk.v@gmail.com>
2018-06-04 20:07:06 +0300

    basic

--- L1.sh
+++ L1.sh
@@ -1,1 +1,42 @@
+#!/usr/bin/env bash
 
+loop ()
+{ 
+
+
+
+
+
+
+
+
+echo "this is function"
+}
+
+
+read -p "Do you want to look throught a file /etc/shadow ? [ Y / N ]: " question
+
+case "$question" in 
+
+	y | Y) if [ `whoami` = root ]
+
+			then loop
+
+			else	echo "You are not root, exit"; exit 0
+
+		fi 
+	;;
+
+	n | N) echo "Exit"; exit 0
+
+	;;
+
+	*) echo "Something Exit"; exit 0
+
+	;;
+
+esac
+
+exit 0
+
+			
